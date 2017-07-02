dc6e84c1e4604b5ff70f96ab4e9b8a2b5018bcb9
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2017-07-02 18:18:25 -0400

    adding test cases

--- polyparser/examples.py
+++ polyparser/examples.py
@@ -3,11 +3,14 @@ from func_util import function_utilities
 
 parser = Parser()
 
+expwriter = function_utilities.Writer("func.xml")
+expwriter.writeexpr("x ^ 3 + x")
+
 expreader = function_utilities.Reader("func.xml")
 expreader.readexpr("hidden")
 
-expwriter = function_utilities.Writer("func.xml")
-expwriter.writeexpr("x ^ 3")
+output = parser.parse(expreader.expression).evaluate({"x": 3})
+output1 = parser.parse(expwriter.expression).evaluate({"x": 2})
 
-output = parser.parse(expwriter.expression).evaluate({"x": 2})
+
 print(output)
