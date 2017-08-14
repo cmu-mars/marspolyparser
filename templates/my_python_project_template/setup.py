6e4b596394ee0fffae9d9218018f32ad7d9c93ea
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2017-08-14 18:57:32 -0400

    added the templates

--- templates/my_python_project_template/setup.py
+++ templates/my_python_project_template/setup.py
@@ -17,7 +17,7 @@ setup(
     author='Pooyan Jamshidi',
     author_email='pooyan.jamshidi@gmail.com',
     url='https://github.com/pooyanjamshidi',
-    acknowledgement='The class parser in this repository is mainly based on js-expression-eval, by Matthew Crumley (email@matthewcrumley.com, http://silentmatt.com/) https://github.com/silentmatt/js-expression-eval and https://github.com/Axiacore/py-expression-eval',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
+
---
+++ templates/test_basic.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from polyparser.poly_parser import Parser
+
+import unittest
+from polyparser.func_util import function_utilities
+from polyparser import poly_parser
+
+class BasicTestSuite(unittest.TestCase):
+    """Basic test cases."""
+
+    def test_read_polynomial(self):
+        expwriter = function_utilities.Writer("func.xml")
+        expwriter.writeexpr("x ^ 2")
+        expreader = function_utilities.Reader("func.xml")
+        expreader.readexpr("hidden")
+
+        self.assertTrue(expreader.expression == "x ^ 2")
+
+        expwriter.writeexpr("x ^ 3")
+        expreader.readexpr("hidden")
+
+        self.assertFalse(expreader.expression == "x ^ 2")
+
+    def test_write_polynomial(self):
+        expwrite = function_utilities.Writer("func.xml")
+        expwrite.writeexpr("x*y")
+
+        self.assertTrue(expwrite.expression == "x*y")
+
+    def test_parser(self):
+        parser = Parser()
+
+        expwrite = function_utilities.Writer("func.xml")
+        expwrite.writeexpr("x * y")
+        expreader = function_utilities.Reader("func.xml")
+        expreader.readexpr("hidden")
+
+        output = parser.parse(expreader.expression).evaluate({"x":2, "y": 4})
+
+        self.assertTrue(output == 8)
+
+    def test_absolute_truth_and_meaning(self):
+        assert True
+
+
+
+# if __name__ == '__main__':
+#     unittest.main()
+
+suite = unittest.TestLoader().loadTestsFromTestCase(BasicTestSuite)
+unittest.TextTestRunner(verbosity=2).run(suite)
